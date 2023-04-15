94c410eb5b0e6cad128cdd6eeb39923db3114606
20233523 <20233523@life.hkbu.edu.hk>
2023-04-16 06:32:12 +0800

    add character and follow light

---
+++ Assets/object/LightFollow.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFollow : MonoBehaviour
+{
+    public GameObject thePlayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(thePlayer.transform);
+    }
+}
